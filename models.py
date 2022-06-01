4669b0c5c24cab33fe643e782b9d15550d701699
Demivability <deus@wraithgod.strangled.net>
2022-06-01 20:34:01 +0300

    Add models.py
    Signed-off-by: Demivability <deus@wraithgod.strangled.net>

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class PublicItem(db.Model):
+    id = db.Column(db.Integer, prinary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    color = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"<PublicItem {self.name}>"
