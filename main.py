225d63d37e54c09e33990afbfd141db378362248
Demivability <deus@wraithgod.strangled.net>
2022-06-01 20:01:31 +0300

    Add main.py
    Signed-off-by: Demivability <deus@wraithgod.strangled.net>

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+#from models import db
+
+app = Flask(__name__)
+#db.init_app(app)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route("/management")
+def management():
+    return "Management Page"
+
+if __name__ == "__main__":
+    app.run()
