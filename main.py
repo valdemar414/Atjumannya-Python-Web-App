100fcdfc6f943e9984d887a995bfbe6da59e1ee4
Demivability <deus@wraithgod.strangled.net>
2022-06-03 11:00:11 +0300

    Implement basic DB operations
    Signed-off-by: Demivability <deus@wraithgod.strangled.net>

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from flask import Flask, request
-from models import db
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, PublicItem
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 db.init_app(app)
+db.create_all(app=app)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html", items=PublicItem.query.all())
+
+@app.route("/create_item", methods=["GET", "POST"])
+def create_item():
+    if request.method == "GET":
+        return render_template("create_item.html")
+    if request.method == "POST":
+        attrs = request.form
+        item = PublicItem(**attrs)
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for("list_items"))
+
+@app.route("/list_items")
+def list_items():
+    items=PublicItem.query.all()
+    empty = False
+    if len(items) == 0:
+        empty = True
+    ctx = {
+        "items": items,
+        "empty": empty
+    }
+    return render_template("list_items.html", **ctx)
+
+@app.route("/delete/item/<id>", methods=["POST"])
+def delete_item(id):
+    item = PublicItem.query.filter_by(id=id).first()
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for("list_items"))
 
 @app.route("/management")
 def management():
