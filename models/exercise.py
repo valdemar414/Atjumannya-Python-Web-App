d958a1c04a18a696d3564c677dd1366e82487967
Derivability <derivability@wraithgod.strangled.net>
2022-06-19 14:01:07 +0300

    Add example_link column to Exercise
    Signed-off-by: Derivability <derivability@wraithgod.strangled.net>

--- models/exercise.py
+++ models/exercise.py
@@ -5,3 +5,4 @@ class Exercise(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80), unique=True, nullable=False)
 	muscle = db.Column(db.String(80), db.ForeignKey(Muscle.name))
+	example_link = db.Column(db.String())
