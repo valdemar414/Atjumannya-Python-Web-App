9d128b67e753d4cc414d07c353139691bd51621d
Derivability <derivability@wraithgod.strangled.net>
2022-06-19 14:24:40 +0300

    Add user reference in Approach model
    Signed-off-by: Derivability <derivability@wraithgod.strangled.net>

--- models/approach.py
+++ models/approach.py
@@ -1,9 +1,11 @@
 from models import db
 from models.exercise import Exercise
+from models.user import User
 
 
 class Approach(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	exercise = db.Column(db.String(80), db.ForeignKey(Exercise.name))
+	user = db.Column(db.Integer, db.ForeignKey(User.id))
 	repetitions = db.Column(db.Integer, nullable=False)
 	weight = db.Column(db.Numeric(3,2), nullable=False)
